c0919da05575f8c90ecdd44ad1196cf04ec2b0d0
zqwxxg <zwon0015@gmail.com>
2025-02-22 15:35:48 +0800

    Added in class ex6

---
+++ In-class_Ex/In-class_Ex06/app.R
@@ -0,0 +1,50 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(tidyverse)
+
+exam <- read.csv("../../data/Exam_data.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel("Pupils Exam Result Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "variable",
+                  label = "Subject:",
+                  choices = c("English" = "ENGLISH",
+                              "Maths" = "MATHS",
+                              "Science" = "SCIENCE"),
+                  selected = "ENGLISH"),
+      sliderInput(inputId = "bins",
+                  label = "Number of Bins",
+                  min = 5,
+                  max = 20,
+                  value = 10)
+      ),
+    mainPanel(
+      plotOutput("distPlot") 
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output){
+  output$distPlot <- renderPlot({
+    ggplot(exam, 
+           aes_string(x = input$variable)) + 
+      geom_histogram(bins = input$bins, 
+                     color="black",  
+                     fill="light blue") 
+  }) 
+} 
+
+# Run the application 
+shinyApp(ui = ui, server = server)
